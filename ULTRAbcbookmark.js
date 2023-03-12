19145a2a2d120b60a642768e2c7d11b4f80b4942
tetris245 <58624501+tetris245@users.noreply.github.com>
2023-03-12 23:58:19 +0100

    Add files via upload

---
+++ ULTRAbcbookmark.js
@@ -0,0 +1,1 @@
+javascript:(function (){let n = document.createElement("script");n.setAttribute("language", "JavaScript");n.setAttribute("crossorigin", "anonymous");n.setAttribute("src", "https://tetris245.github.io/ultrabc.github.io/ULTRAbc.js?_=" + Date.now());document.head.appendChild(n);})();
---
+++ ULTRAbcloader.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name ULTRAbc
+// @namespace https://www.bondageprojects.com/
+// @version 1.0
+// @description Everything you'll ever need for BC
+// @author Nemesea
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match http://localhost:*/*
+// @icon data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+setTimeout(
+	function () {
+			let n = document.createElement("script");
+			n.setAttribute("language", "JavaScript");
+			n.setAttribute("crossorigin", "anonymous");
+			n.setAttribute("src", "https://tetris245.github.io/ultrabc.github.io/ULTRAbc.js?_=" + Date.now());
+			n.onload = () => n.remove();
+			document.head.appendChild(n);
+		}
+);
