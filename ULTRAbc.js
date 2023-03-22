3ef56d4d98a16440fd3d66130e076e72b299fcf9
tetris245 <58624501+tetris245@users.noreply.github.com>
2023-03-22 10:47:28 +0100

    Less lines for conditions

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -71,7 +71,7 @@ var bcModSDK=function(){"use strict";const e="1.1.0";function o(e){alert("Mod ER
                 if ((text1.startsWith(".")) && (window.BCX_Loaded == true)) {
                     var text2 = text1;
                     var tsp = 1;
-		} else if (text1.startsWith("!")) {
+		} else if ((text1.startsWith("!")) || (text1.startsWith("/")) || (text1.startsWith("(")) || (text1.startsWith(":"))) {
                     var text2 = text1;
                     var tsp = 1;
 		} else if (text1.startsWith("*")) {
@@ -81,16 +81,7 @@ var bcModSDK=function(){"use strict";const e="1.1.0";function o(e){alert("Mod ER
                 } else if ((text1.startsWith("@")) && (window.MBCHC)) {
                     var text2 = text1;
                     var tsp = 1;
-		    ChatRoomSetTarget(null);
-                } else if (text1.startsWith("/")) {
-                    var text2 = text1;
-                    var tsp = 1;
-		} else if (text1.startsWith("(")) {
-                    var text2 = text1;
-                    var tsp = 1;
-		} else if (text1.startsWith(":")) {
-                    var text2 = text1;
-                    var tsp = 1;	
+		    ChatRoomSetTarget(null);	
                 } else {
 		    var tsp = 0;
                     if (this.BabyTalkOn == true) {
