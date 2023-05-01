727f8db1f84da81b16c1b823103d0afe33f586c6
tetris245 <58624501+tetris245@users.noreply.github.com>
2023-05-01 15:06:01 +0200

    Update ULTRAbc.js

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -7641,7 +7641,6 @@ var bcModSDK=function(){"use strict";const e="1.1.0";function o(e){alert("Mod ER
         }
     }])
 
-
     CommandCombine([{
         Tag: 'title',
         Description: "(title): chooses a new title.",
