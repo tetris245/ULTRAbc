779878822e0fe7d8d9f7c6f5702121a87b417d79
tetris245 <58624501+tetris245@users.noreply.github.com>
2023-07-14 16:46:27 +0200

    Update ULTRAbc.js

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -5966,7 +5966,7 @@ var bcModSDK=function(){"use strict";const e="1.1.0";function o(e){alert("Mod ER
                         Type: "Action",
                         Dictionary: [{
                             Tag: "Beep",
-                            Text: "Magical lasers make appear locks on " + tgpname + "'s body."
+                            Text: message
                         }]
                     });
                     mn = Player.MemberNumber;
