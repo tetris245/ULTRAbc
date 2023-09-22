4fb34744d3c8dddacb638f69830d17e011debcd5
tetris245 <58624501+tetris245@users.noreply.github.com>
2023-09-23 00:52:16 +0200

    Update ULTRAbc.js

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -4708,7 +4708,6 @@ var bcModSDK=function(){"use strict";const e="1.1.0";function o(e){alert("Mod ER
                 var fame = args;
                 if ((fame > 199) && (fame < 1001) && (fame != highfame)) {
                     highfame = fame;
-                    M_MOANER_saveControls();
                     ChatRoomSendLocal(
                         "<p style='background-color:#5fbd7a'><b>ULTRAbc</b>: Fame level changed for the high fame mode of the Bondage Club Card Game.</p>"
                     );
