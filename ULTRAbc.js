6f2ddbfc7284e5168e2c1ab4c8e050e46d0e3240
tetris245 <58624501+tetris245@users.noreply.github.com>
2023-10-14 13:07:48 +0200

    Update of updateBackground function

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -1439,6 +1439,7 @@ var bcModSDK=function(){"use strict";const e="1.1.0";function o(e){alert("Mod ER
             Game: ChatRoomData.Game,
             Private: ChatRoomData.Private,
             Locked: ChatRoomData.Locked,
+	    Custom: ChatRoomData.Custom,
         };
         ServerSend("ChatRoomAdmin", {
             MemberNumber: Player.ID,
