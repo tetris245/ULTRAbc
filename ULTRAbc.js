9928e3948cb0b4675470413700096213df09c903
tetris245 <58624501+tetris245@users.noreply.github.com>
2023-11-24 22:27:43 +0100

    Correction for xstatus mbs command

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -12107,7 +12107,7 @@ var bcModSDK=function(){"use strict";const e="1.1.0";function o(e){alert("Mod ER
                         showEbchWelcomeStatus();
                     }
 		} else if (addon == "mbs") {
-                    if (Player.OnlineSettings.MBS != null) {
+                    if (Player.OnlineSharedSettings.MBSVersion != null) {
                         MBSver = Player.OnlineSharedSettings.MBSVersion;
                         var stringMBSver1 = MBSver;
                         var stringMBSver2 = stringMBSver1.split(".");
