d891c08d208f731fa9967d51b2326b22a89bfe28
tetris245 <58624501+tetris245@users.noreply.github.com>
2023-11-26 11:52:10 +0100

    Version 2.4

--- ULTRAbcloader.user.js
+++ ULTRAbcloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name ULTRAbc
 // @namespace https://www.bondageprojects.com/
-// @version 2.3
+// @version 2.4
 // @description Everything you'll ever need for BC
 // @author Nemesea
 // @match https://bondageprojects.elementfx.com/*
