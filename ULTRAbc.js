8b5eea067b3766cb802820069a4a65547d8a2b0c
tetris245 <58624501+tetris245@users.noreply.github.com>
2023-12-14 00:05:29 +0100

    Correction

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -1724,7 +1724,7 @@ var bcModSDK=function(){"use strict";const e="1.1.0";function o(e){alert("Mod ER
             Game: ChatRoomData.Game,
             Private: ChatRoomData.Private,
             Locked: ChatRoomData.Locked,    
-            MapData: ChatAdminMapData,
+            MapData: ChatRoomData.MapData,
             Custom: ChatRoomData.Custom,
         };
         ServerSend("ChatRoomAdmin", {
