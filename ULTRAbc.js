3e0e9db5eb4eec90c93a0c7bb0c026af75f139d0
tetris245 <58624501+tetris245@users.noreply.github.com>
2024-01-06 18:28:10 +0100

    Support for message starting with \

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -724,20 +724,17 @@ var bcModSDK=function(){"use strict";const e="1.1.0";function o(e){alert("Mod ER
                     var text2 = text1;
                     var tsp = 1;
                     ChatRoomSetTarget(null);
-                } else if (text1.startsWith("*")) {
+                } else if ((text1.startsWith("*")) || (text1.startsWith("/"))) {
                     var text2 = text1;
                     var tsp = 1;
                     ChatRoomSetTarget(null);
-                } else if (text1.startsWith("/")) {
-                    var text2 = text1;
-                    var tsp = 1;
-                    if (!text1.startsWith("//")) {
-                        ChatRoomSetTarget(null);
-                    }
                 } else if ((text1.startsWith("@")) && (window.MBCHC)) {
                     var text2 = text1;
                     var tsp = 1;
                     ChatRoomSetTarget(null);
+		} else if (text1.startsWith("\\")) {
+                    var text2 = text1.replaceAt(0, "\u200b");
+                    var tsp = 1;
                 } else {
                     var tsp = 0;
                     if (this.BabyTalkOn == true) {
