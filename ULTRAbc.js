c195efa4f2514501baf5c4f027db64ce08832504
tetris245 <58624501+tetris245@users.noreply.github.com>
2024-02-12 16:01:10 +0100

    Update of invisible command

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -6862,6 +6862,7 @@ var bcModSDK=function(){"use strict";const e="1.1.0";function o(e){alert("Mod ER
                         "Activity",
                         "Blush",
                         "BodyLower",
+			"BodyMarkings",
                         "BodyUpper",
                         "Bra",
                         "Bracelet",
