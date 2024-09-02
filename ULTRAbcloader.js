38efa8a86db632da13ecca026d8bc866ff4315a4
tetris245 <58624501+tetris245@users.noreply.github.com>
2024-09-02 16:08:37 +0200

    Version 3.6

--- ULTRAbcloader.js
+++ ULTRAbcloader.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name ULTRAbc
 // @namespace https://www.bondageprojects.com/
-// @version 3.5.1
+// @version 3.6
 // @description Everything you'll ever need for BC
 // @author Nemesea
 // @match https://bondageprojects.elementfx.com/*
