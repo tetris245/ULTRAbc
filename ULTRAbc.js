05ca79030b7104cad23f8104c4d529ddcdcf227f
tetris245 <58624501+tetris245@users.noreply.github.com>
2024-09-12 12:00:32 +0200

    Update ULTRAbc.js

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -1052,10 +1052,10 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                 ElementValue("InputChat", text1);
             } else if ((msg.startsWith("!")) && (Player.ChatSettings.OOCAutoClose == true)) {
                 var lr = SpeechGetOOCRanges(msg).pop();
-                if ((lr !== undefined) &&   
-                    (msg.charAt(lr.start + lr.length - 1) !== ")") &&
-		    (lr.start + lr.length === msg.length) && 
-		    (lr.length !== 1)) {
+                if ((lr != undefined) &&   
+                    (msg.charAt(lr.start + lr.length - 1) != ")") &&
+		    (lr.start + lr.length == msg.length) && 
+		    (lr.length != 1)) {
                     var text1 = msg + ")";
                 } else {
                      var text1 = msg; 
