8fda402aff0341611b87e71e68392a78763de7b4
tetris245 <58624501+tetris245@users.noreply.github.com>
2024-09-25 21:59:21 +0200

    Version 3.7

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name ULTRAbc
 // @namespace https://www.bondageprojects.com/
-// @version 3.6.1
+// @version 3.7
 // @description Everything you'll ever need for BC
 // @author Nemesea
 // @match https://bondageprojects.elementfx.com/*
@@ -26,8 +26,8 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
         return;
     }
 
-    const UBCver = "3.6.1";
-    const UBCver0 = "3.6";
+    const UBCver = "3.7";
+    const UBCver0 = "3.6.1";
     const modApi = bcModSDK.registerMod({
         name: 'ULTRAbc',
         fullName: 'Ultra Bondage Club',
