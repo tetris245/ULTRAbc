9504dea6580d5237767a97ab6ace76bf62c836b7
tetris245 <58624501+tetris245@users.noreply.github.com>
2024-12-30 07:49:34 +0100

    More info about tint effect

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -1859,7 +1859,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                     "Input a number between 0 and 3 to select one of these forced 'permanent' tint effect levels: 0 No tint effect - 1 Light tint effect - 2 Medium tint effect - 3 Heavy tint effect.", 60
                 );
                 addMenuInput(200, "Tint effect color (format #000000):", "tintcolor", "InputTintColor",
-                    "Input a color code in the hexadecimal format #000000 to tint many backgrounds in the Bondage Club. Don't forget to select a tint effect level too! ", 60
+                    "Input a color code in the hexadecimal format #000000 to apply a tint effect almost everywhere in the Bondage Club. Don't forget to select a tint effect level too! The tint effect will also be applied on pages created by most add-ons. Known exceptions are BCX and Echo's mod. Also you will notice the special case of MBS mod. The final color can be different when mixed with a Themed color.", 60
                 );
             }
 
