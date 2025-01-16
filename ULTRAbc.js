e388d944d711233219307ce1d18a42dec0f325e3
tetris245 <58624501+tetris245@users.noreply.github.com>
2025-01-16 09:30:16 +0100

    No tint effect on MBS screens if tint level = 0

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -1903,7 +1903,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                 addMenuInput(200, "Tint effect color (format #000000):", "tintcolor", "InputTintColor",
                     "Input a color code in the hexadecimal format #000000 to apply a tint effect almost everywhere in the Bondage Club. Don't forget to select a tint effect level too! The tint effect will also be applied on pages created by most add-ons. Known exceptions are BCX, MBS and Echo's mod. The final color can be different when mixed with a Themed color.", 60
                 );
-		let mbsmsg = "When enabled, the tint color will be used as background color for the central part of MBS screens. If you disable it later, the restored color will correspond to the default MBS color or the main Themed color. This setting is not available when MBS is not detected."; 
+		let mbsmsg = "When enabled, the tint color will be used as background color for the central part of MBS screens. If you disable it later, the restored color will correspond to the default MBS color or the main Themed color. This setting is not available when MBS is not detected. It is without any effect when the tint level is 0."; 
                 let mbb = 0;
                 let list = PreferenceExtensionsDisplay;
                 for (let i = 0; i < list.length; i++) {
@@ -3368,7 +3368,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
             for (let i = 0; i < list.length; i++) {
                if (list[i].Button == "MBS Settings") mbb = 1; 
             }
-            if ((name == "Extensions") && (mbb == 1)) {
+            if ((name == "Extensions") && (mbb == 1) && (tintlevel != 0)) {
                 if (tintmbs == true) tintMbsColors();
                 if (tintmbs == false) untintMbsColors();
             }
