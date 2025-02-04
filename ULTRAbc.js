7568120dbb25c3759da2705adf6ce6b90b552bf4
tetris245 <58624501+tetris245@users.noreply.github.com>
2025-02-04 06:20:37 +0100

    bgshow replaced by bgshow1 + bgshow2

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -7957,8 +7957,8 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
     }])
 
     CommandCombine([{
-        Tag: 'bgshow',
-        Description: "(bgnumber): displays link to a specific standard background. When used with WCE feature, the picture can be embedded in the chat.",   
+        Tag: 'bgshow1',
+        Description: "(bgnumber): sends locally clickable link to a specific standard background. When used with WCE feature, the picture can be embedded in the chat.",   
         Action: (args) => {
             let BCver = GameVersion;
             if (BCver.includes("Beta")) {
@@ -7968,12 +7968,53 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                 BCver = "R" + beta3;
             }
             if (args === "") {
-                let msg = "The bgshow command must be followed by a number (use /bglist to get available numbers).";
+                let msg = "The bgshow1 command must be followed by a number (use /bglist to get available numbers).";
                 infomsg(msg);
             } else {
                 if ((args > -1) && (args < BackgroundsList.length)) {
                     let name = BackgroundsList[args].Name;
-                    let msg = "https://www.bondage-europe.com/" + BCver + "/BondageClub/Backgrounds/" + name +".jpg";
+                    let url = 'https://www.bondage-europe.com/' + BCver + '/BondageClub/Backgrounds/' + name + '.jpg';
+                    let bgc = 0;
+                    if (Player.FBC != undefined) {
+                        let str = Player.ExtensionSettings.FBC;
+                        let d = LZString.decompressFromBase64(str);
+                        let FBCdata = {};
+                        let decoded = JSON.parse(d);
+                        FBCdata = decoded
+                        if (FBCdata.augmentChat) bgc = 1;
+                    }
+                    if (bgc == 0) {
+                        let msg = '<a href="'+url+'" target="_blank">'+url+'</a>';
+                        let txt = "( " + msg + " )";
+                        infomsg(txt);
+                    } else {
+                        let msg = "( " + url + " )";
+                        ChatRoomSendLocal(msg); 
+                    }
+                }
+            } 
+        }
+    }])
+
+    CommandCombine([{
+        Tag: 'bgshow2',
+        Description: "(bgnumber): sends in chat link to a specific standard background. When used with WCE feature, the link is clickable and the picture can be embedded in the chat.",   
+        Action: (args) => {
+            let BCver = GameVersion;
+            if (BCver.includes("Beta")) {
+                let beta1 = BCver.slice(0, 4);
+                let beta2 = beta1.slice(-3);
+                let beta3 = beta2 - 1;
+                BCver = "R" + beta3;
+            }
+            if (args === "") {
+                let msg = "The bgshow2 command must be followed by a number (use /bglist to get available numbers).";
+                infomsg(msg);
+            } else {
+                if ((args > -1) && (args < BackgroundsList.length)) {
+                    let name = BackgroundsList[args].Name;
+                    let url = 'https://www.bondage-europe.com/' + BCver + '/BondageClub/Backgrounds/' + name + '.jpg';                  
+                    let msg = "( " + url + " )";
                     ChatRoomSendChatMessage(msg);
                 }
             } 
@@ -13949,7 +13990,8 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                 let msg = "Settings commands - * = more info when using\n" +
                     "<b>/bg4</b> (screen) (background) = selects a standard background for the Club Card Game, Friend List, Main Hall, Private Room (SP) or Timer Cell. *\n" +
                     "<b>/bglist</b> displays the list of all available standard backgrounds.\n" +
-		    "<b>/bgshow</b> (bgnumber) = displays link to a specific standard background. Embedding possible if used with WCE feature.\n" +  
+		    "<b>/bgshow1</b> (bgnumber) = displays locally clickable link to a specific standard background. Embedding possible if used with WCE feature.\n" + 
+		    "<b>/bgshow2</b> (bgnumber) = displays in chat link to a specific standard background. Clickable link and embedding possible if used with WCE feature.\n" + 
                     "<b>/killpar</b> = kills UBC/Moaner parameters saved locally.\n" +
                     "<b>/message</b> (option) (message) = creates custom messages for specific command. *";
                 infomsg(msg);
