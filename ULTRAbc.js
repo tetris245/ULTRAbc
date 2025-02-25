9695642b561c49301eb4bd396316f275a0e9e289
tetris245 <58624501+tetris245@users.noreply.github.com>
2025-02-25 21:37:01 +0100

    Better text

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -1822,7 +1822,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                     "Input a number between 0 and 9 to select a device trap: 0 No device trap - 1 Bondage Bench - 2 Coffin - 3 Display Frame - 4 Kennel - 5 Locker - 6 Trolley - 7 Wooden Box - 8 X-Cross - 9 ALL THE DEVICE TRAPS. When a trap is enabled, you will be automatically bound if you walk on the device!", 6
                 );
 		addMenuCheckbox(64, 64, "Enable full vision/hearing in maps: ", "mapfull",
-                    "When enabled, you can see the entire map rooms without fog and there's no any limitation to your hearing. Notes: the /mapfrog command, that enables/disables the fog only in the current map room, is without any effect if this setting is active. If you have used /mapfog before enabling this setting, the fog will come back when disabling it.", false, 140
+                    "When enabled, you can see the entire map rooms without fog and there's no any limitation to your hearing. Notes: the /mapfog command, that enables/disables the fog only in the current map room, is without any effect if this setting is active. If you don't have used /mapfog to remove the fog before enabling this setting, the fog will come back when disabling it.", false, 140
                 );
 		addMenuCheckbox(64, 64, "Enable magic walk in maps: ", "mapcheat",
                     "When enabled, you can go everywhere in the maps, also pass through walls, even while not being an administrator!", false, 140
