3f3a0fa57070c818ba3611c8090199f041d70084
tetris245 <58624501+tetris245@users.noreply.github.com>
2025-04-10 03:23:50 +0200

    Hotfix 3 for the case of a brand new player

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -5173,44 +5173,46 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
             }
         }
         let str = Player.ExtensionSettings.BCX;
-        if (/^[0-9]+:/.test(str)) {
-            const parts = str.split(":");
-            const saveVersion = Number.parseInt(parts[0], 10);
-            if ((saveVersion === 2) && (parts.length === 3)) {
-	        str = parts[1];
-                let d = LZString.decompressFromBase64(str);
-                let BCXdata = {};
-                let decoded = JSON.parse(d);
-                BCXdata = decoded;
-                let keys1 = Object.keys(BCXdata);   
-                if (keys1.includes("conditions")) { 
-                    let keys2 = Object.keys(BCXdata.conditions);
-                    if (keys2.includes("rules")) {
-                        let keys3 = Object.keys(BCXdata.conditions.rules);  
-                        if (keys3.includes("conditions")) {
-                            let keys4 = Object.keys(BCXdata.conditions.rules.conditions); 
-                            if (keys4.includes("speech_restrict_whisper_send")) {       
-                                BCXwh1 = BCXdata.conditions.rules.conditions.speech_restrict_whisper_send; 
-                                if (BCXwh1.active) {
-                                    wh1 = 1;
-                                    wh1data = BCXwh1.data.customData.minimumPermittedRole;
-                                    if (wh1data == 1) bcxlist = wh1ex;
-                                    wh2ex = (BCXdata.owners).concat(wh1ex);
-                                    if (wh1data == 2) bcxlist = wh2ex;
-                                    wh3ex = lovers.concat(wh2ex);
-                                    if (wh1data == 3) bcxlist = wh3ex;
-                                    wh4ex = (BCXdata.mistresses).concat(wh3ex);   
-                                    if (wh1data == 4) bcxlist = wh4ex;
-                                    wh5ex = (Player.WhiteList).concat(wh4ex);
-                                    if (wh1data == 5) bcxlist = wh5ex;
-                                    wh6ex = (Player.FriendList).concat(wh5ex);
-                                    if (wh1data == 6) bcxlist = wh6ex;
-                                    if (wh1data == 7) wh1 = 0; 
-                                    if (ChatRoomTargetMemberNumber == Player.Ownership.MemberNumber) {
-                                        wh1 = 0;
-                                    } else {
-                                        if ((wh1data > 1) && (wh1data < 7)) {
-                                            if (bcxlist.includes(ChatRoomTargetMemberNumber)) wh1 = 0; 
+        if (str != undefined) {
+            if (/^[0-9]+:/.test(str)) {
+                const parts = str.split(":");
+                const saveVersion = Number.parseInt(parts[0], 10);
+                if ((saveVersion === 2) && (parts.length === 3)) {
+	            str = parts[1];
+                    let d = LZString.decompressFromBase64(str);
+                    let BCXdata = {};
+                    let decoded = JSON.parse(d);
+                    BCXdata = decoded;
+                    let keys1 = Object.keys(BCXdata);   
+                    if (keys1.includes("conditions")) { 
+                        let keys2 = Object.keys(BCXdata.conditions);
+                        if (keys2.includes("rules")) {
+                            let keys3 = Object.keys(BCXdata.conditions.rules);  
+                            if (keys3.includes("conditions")) {
+                                let keys4 = Object.keys(BCXdata.conditions.rules.conditions); 
+                                if (keys4.includes("speech_restrict_whisper_send")) {       
+                                    BCXwh1 = BCXdata.conditions.rules.conditions.speech_restrict_whisper_send; 
+                                    if (BCXwh1.active) {
+                                        wh1 = 1;
+                                        wh1data = BCXwh1.data.customData.minimumPermittedRole;
+                                        if (wh1data == 1) bcxlist = wh1ex;
+                                        wh2ex = (BCXdata.owners).concat(wh1ex);
+                                        if (wh1data == 2) bcxlist = wh2ex;
+                                        wh3ex = lovers.concat(wh2ex);
+                                        if (wh1data == 3) bcxlist = wh3ex;
+                                        wh4ex = (BCXdata.mistresses).concat(wh3ex);   
+                                        if (wh1data == 4) bcxlist = wh4ex;
+                                        wh5ex = (Player.WhiteList).concat(wh4ex);
+                                        if (wh1data == 5) bcxlist = wh5ex;
+                                        wh6ex = (Player.FriendList).concat(wh5ex);
+                                        if (wh1data == 6) bcxlist = wh6ex;
+                                        if (wh1data == 7) wh1 = 0; 
+                                        if (ChatRoomTargetMemberNumber == Player.Ownership.MemberNumber) {
+                                            wh1 = 0;
+                                        } else {
+                                            if ((wh1data > 1) && (wh1data < 7)) {
+                                                if (bcxlist.includes(ChatRoomTargetMemberNumber)) wh1 = 0; 
+                                            }
                                         }
                                     }
                                 }
