f0dbaad0b0f73d48d41b204e38552bd384fc8fbf
tetris245 <58624501+tetris245@users.noreply.github.com>
2025-11-11 22:09:17 +0100

    Better OutChat function

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -5596,13 +5596,8 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                 Game: Player.Game
             }, true);
         }
-        ChatRoomSetLastChatRoom("");
-        ServerSend("ChatRoomLeave", "");
-        CommonSetScreen("Online", "ChatSearch");
-        ChatRoomClearAllElements();
-        OnlineGameName = "";
-        ChatSearchGame = "";
-        ChatRoomData = null;
+        ChatRoomLeave();
+        CommonSetScreen("Online", "ChatSearch");    
     }
 
     function OutClick() {
@@ -16452,6 +16447,3 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
     }])
 
 })();
-
-
-
