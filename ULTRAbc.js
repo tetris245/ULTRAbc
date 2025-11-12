00e5dadcadc9184e68669e8a7da14aaf54ffd83a
tetris245 <58624501+tetris245@users.noreply.github.com>
2025-11-12 20:11:47 +0100

    Better fix for tooltips from 7th row

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -50,7 +50,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
             display: flex;
             flex-direction: row;
             justify-content: space-between;
-            z-index: 10;
+            z-index: inherit;
         }
         .chat-search-room-bottom-section {
             width: fit-content;
@@ -16447,6 +16447,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
     }])
 
 })();
+
 
 
 
