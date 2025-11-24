4e62b459cbda365f49358a5b142d3edef07cad1d
tetris245 <58624501+tetris245@users.noreply.github.com>
2025-11-24 02:43:24 +0100

    DOGS/no-escape hotfix

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -918,6 +918,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                 if (csname == null || csname == undefined) csname = "Introduction";
                 if (dogsforbid == null || dogsforbid == undefined) dogsforbid = false;
 				if (dogsforced == null || dogsforced == undefined) dogsforced = false;
+				if (dogsforced == false) dogsforbid = false;
                 if (dolltalk == null || dolltalk == undefined) dolltalk = false;
                 if (extbuttons == null || extbuttons == undefined) extbuttons = false;
                 if (extrainfo == null || extrainfo == undefined) extrainfo = false;
@@ -1722,6 +1723,10 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                     Player.RestrictionSettings.BypassNPCPunishments = false;
                 } else {
                     Player.RestrictionSettings.BypassNPCPunishments = true;
+                }
+				if (dogsforced == false) {
+                    dogsforbid = false;
+                    M_MOANER_saveControls();              
                 }
                 if ((noescape == true) && (unrestrict == 2)) {
                     Player.UBC.ubcSettings.utotal = false;
@@ -8424,8 +8429,12 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
     ];
 
     function DOGSsettings() {
+		dogsforbid = false;
+        if (dogsforced == false) {
+           M_MOANER_saveControls();
+           return;
+        }
         if (dogsforced == true) {
-            dogsforbid = false;
             let DOGS = Player.ExtensionSettings.DOGS;
             if (DOGS) {
                 let DOGSdata = JSON.parse(LZString.decompressFromBase64(DOGS));
@@ -16669,5 +16678,6 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
     }])
 
 })();
+
 
 
