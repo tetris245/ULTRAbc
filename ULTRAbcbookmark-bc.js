899b378c132352eb1e5c6601a2f8813e0beaa32d
tetris245 <58624501+tetris245@users.noreply.github.com>
2026-03-20 06:07:16 +0100

    Delete ULTRAbcbookmark-bc.js

--- ULTRAbcbookmark-bc.js
+++
@@ -1,1 +0,0 @@
-javascript:(function (){let n = document.createElement("script");n.setAttribute("language", "JavaScript");n.setAttribute("crossorigin", "anonymous");n.setAttribute("src", "https://tetris245.github.io/ultrabc.github.io/ULTRAbc-ch.js?_=" + Date.now());document.head.appendChild(n);})();
