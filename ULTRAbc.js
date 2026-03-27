e01256216bb5a5315099419c10ebcef032a98d8c
tetris245 <58624501+tetris245@users.noreply.github.com>
2026-03-27 12:44:56 +0100

    Adjustment

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -1972,7 +1972,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                             Game: Player.Game
                         }, true);
                     },
-                    "This setting is a toggle. You can add or remove all the reward extra cards. Note that also the extra cards you acquired by the normal way will be removed when this setting switches to the remove action.", 160
+                    "This setting is a toggle. You can add or remove all the reward extra cards. Note that also the extra cards you acquired by the normal way will be removed when this setting switches to the remove action.", 140
                 );
                 addMenuCheckbox(64, 64, "Always access to layering menus: ", "layerall",
                     "With this option, you will always have access to layering menus, even if bindings and locks restrain your moves! Note: If you don't want that other players make layering based changes to your character, you can enable a specific option in the WCE mod.", false, 160
