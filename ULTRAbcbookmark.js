cee29cd07481cb2e26a3c2b8e92fa846eb7dd426
tetris245 <58624501+tetris245@users.noreply.github.com>
2026-03-29 23:53:15 +0200

    Update ULTRAbcbookmark.js

--- ULTRAbcbookmark.js
+++ ULTRAbcbookmark.js
@@ -1,1 +1,1 @@
-javascript:(function (){let n = document.createElement("script");n.setAttribute("language", "JavaScript");n.setAttribute("crossorigin", "anonymous");n.setAttribute("src", "https://tetris245.github.io/ultrabc.github.io/ULTRAbc.js?_=" + Date.now());document.head.appendChild(n);})();
+javascript:(function (){let n = document.createElement("script");n.setAttribute("language", "JavaScript");n.setAttribute("crossorigin", "anonymous");n.setAttribute("src", "https://tetris245.github.io/ultrabc.github.io/ULTRAbc-en.js?_=" + Date.now());document.head.appendChild(n);})();
