04a075c878162e370792f9d39a792a3914a7db9b
tetris245 <58624501+tetris245@users.noreply.github.com>
2026-04-01 00:08:19 +0200

    Login buttons instead of selection buttons (2)

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -73,10 +73,11 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
 
     function ULTRALoginRun() {
         modApi.hookFunction('LoginRun', 4, (args, next) => {
-            DrawText("Select", 1850, 440, "White", "Black");
-            DrawText("UBC", 1850, 480, "White", "Black");
-            DrawText("Language", 1850, 520, "White", "Black");
-            DrawButton(1810, 560, 90, 90, "UBC ENGLISH", "White", "", "");
+            DrawText("UBC users", 1850, 400, "White", "Black");
+            DrawText("need to click", 1850, 440, "White", "Black");
+            DrawText("one of these", 1850, 480, "White", "Black");            
+            DrawText("buttons", 1850, 520, "White", "Black");
+            DrawButton(1810, 550, 90, 90, "UBC ENGLISH", "White", "", "");
             DrawButton(1810, 670, 90, 90, "UBC CHINESE", "White", "", "");
             DrawButton(1810, 790, 90, 90, "UBC SPANISH", "White", "", "");
             return next(args);
