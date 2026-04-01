691305e61ab5506953120b1bbeb737e7e888e8ea
tetris245 <58624501+tetris245@users.noreply.github.com>
2026-04-01 11:39:06 +0200

    Change registering in modSDK

--- ULTRAbc.js
+++ ULTRAbc.js
@@ -26,7 +26,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
 
 (function() {
     const modApi = bcModSDK.registerMod({
-        name: 'ULTRAbc',
+        name: 'ULTRAbc-manager',
         fullName: 'Ultra Bondage Club',
         version: "5.9",
         repository: 'https://github.com/tetris245/ULTRAbc',
