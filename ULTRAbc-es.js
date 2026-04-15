2b029b74b6416d7ceddbd0f55dcd542922686c2c
tetris245 <58624501+tetris245@users.noreply.github.com>
2026-04-15 16:44:33 +0200

    Update of AltChatSearchToggle

--- ULTRAbc-es.js
+++ ULTRAbc-es.js
@@ -6190,7 +6190,6 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                 ElementValue("InputFilter", ChatSearchFilterTermsTemp);
                 ChatSearchMode = "Filter";
                 ChatSearchQuery("");
-                ChatSearchQuerySort();
                 break;
             case "Filter":
                 ChatSearchRoomGrid?.classList.remove("chat-search-filter-search");
