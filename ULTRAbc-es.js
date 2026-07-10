97c1e96fd4ebc952b4e8756d269f74c65c853c38
tetris245 <58624501+tetris245@users.noreply.github.com>
2026-07-10 12:42:37 +0200

    Hooks optimisation (15)

--- ULTRAbc-es.js
+++ ULTRAbc-es.js
@@ -3209,7 +3209,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
         return next(args);
     });
 
-    modApi.hookFunction('AsylumEntranceRun', 4, (args, next) => {
+    modApi.hookFunction('AsylumEntranceRun', 4, async (args, next) => {
         TintsEffect();
         DrawText("Salas de Chat", 130, 530, "White", "Black");
         if (asylumlimit == false) {
@@ -3221,7 +3221,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
         return next(args);
     });
 
-    modApi.hookFunction('AsylumMeetingRun', 4, (args, next) => {
+    modApi.hookFunction('AsylumMeetingRun', 4, async (args, next) => {
         TintsEffect();
         if (minigame == "asylum") {
             minigame = "";
@@ -3247,32 +3247,26 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
     });
 
     //Bondage Brawl
-    function ULTRAPlatformAttack() {
-        modApi.hookFunction('PlatformAttack', 4, (args, next) => {
-            if (magiccheat == true) BrawlCheat();
-            next(args);
-        });
-    }
+	modApi.hookFunction('PlatformAttack', 4, (args, next) => {
+        if (magiccheat == true) BrawlCheat();
+        return next(args);
+    });
 
-    function ULTRAPlatformDialogEvent() {
-        modApi.hookFunction('PlatformDialogEvent', 4, (args, next) => {
-            if (magiccheat == true) BrawlCheat();
-            next(args);
-        });
-    }
+    modApi.hookFunction('PlatformDialogEvent', 4, (args, next) => {
+        if (magiccheat == true) BrawlCheat();
+        return next(args);
+    });
 
     //Cafe
-    async function ULTRACafeRun() {
-        modApi.hookFunction('CafeRun', 4, (args, next) => {
-            TintsEffect();
-            if (minigame == "cafe") {
-                minigame = "";
-                M_MOANER_saveControls();
-            }
-            next(args);
-        });
-    }
-
+    modApi.hookFunction('CafeRun', 4, async (args, next) => {
+        TintsEffect();
+        if (minigame == "cafe") {
+            minigame = "";
+            M_MOANER_saveControls();
+        }
+        return next(args);
+    });
+ 
     //Chat Room (+ name/nickname/pronouns management for player)
     modApi.hookFunction('AfkTimerReset', 4, async (args, next) => {
         lastAfkMessageTime = 0;      
