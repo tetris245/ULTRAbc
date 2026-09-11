adbb186dfbe6bea03ba604effb867cec5502b66b
tetris245 <58624501+tetris245@users.noreply.github.com>
2026-09-11 03:30:44 +0200

    Update 2 for the maproom and mapz commands

--- ULTRAbc-ch.js
+++ ULTRAbc-ch.js
@@ -12896,7 +12896,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                         ChatRoomSendLocal(`X = ${X} - Y = ${Y} - ${exinfo}`);
                         keysinfo(Player);
                     } else {
-                        if (!ChatRoomData.BlockCategory.includes("BlockLocationSharing")) {
+                        if ((!ChatRoomData.BlockCategory.includes("BlockLocationSharing")) || (mapfull3)) {
                             ChatRoomSendLocal(`X = ${X} - Y = ${Y} - ${exinfo}`);
                         } else {
                             ChatRoomSendLocal(`Location Sharing blocked - ${exinfo}`);
@@ -13002,7 +13002,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                 ChatRoomSendLocal(`X = ${mapData.Pos?.X ?? "?"} - Y = ${mapData.Pos?.Y ?? "?"} - ${exinfo}`);
                 keysinfo(Player);
             } else {
-                if (!ChatRoomData.BlockCategory.includes("BlockLocationSharing")) {
+                 if ((!ChatRoomData.BlockCategory.includes("BlockLocationSharing")) || (mapfull3)) {
                     ChatRoomSendLocal(`X = ${mapData.Pos?.X ?? "?"} - Y = ${mapData.Pos?.Y ?? "?"} - ${exinfo}`);
                  } else {
                      ChatRoomSendLocal(`Location Sharing blocked - ${exinfo}`);
