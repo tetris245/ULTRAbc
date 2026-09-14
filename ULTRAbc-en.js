314410a78d9d2ebddca66b8229a8fee10746ce28
tetris245 <58624501+tetris245@users.noreply.github.com>
2026-09-14 02:53:45 +0200

    Update ULTRAbc-en.js

--- ULTRAbc-en.js
+++ ULTRAbc-en.js
@@ -3032,7 +3032,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                 addMenuInput(200, "Control item animation (0-2):", "animstate", "InputAnimControl",
                     "Input a number between 0 and 2 to control animation integrated in some items such as the futuristic crate: 0 No control - 1 Partial control (2 updates per second) - 2 Full control (no animation). Important note: alteration of the item animation can lead in some cases to unexpected effects, such as graphical issues, bugs or discrepancies. Use it only if you're experiencing lagging problems.", 65
                 );
-				addMenuCheckbox(64, 64, "Disable gray effect in wardobe: ", "nograywr", 
+				addMenuCheckbox(64, 64, "Disable gray effect in wardrobe: ", "nograywr", 
                     "By default, BC applies a gray effect on the top bar of the wardrobe. If you don't like that, just select this setting!", false, 200
                 );
                 let pmsg = "By default, BC adds messages and pink effect when you are very aroused and will probably have an orgasm. If you don't like that, this UBC setting will make you happy! Note: It is not available when the LSCG Splatter feature is detected as enabled.";
