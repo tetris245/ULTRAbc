bfabc6eb38a76d456a0638146fa4105c9b7b2db3
tetris245 <58624501+tetris245@users.noreply.github.com>
2026-09-25 13:42:19 +0200

    Hotfix - typos

--- ULTRAbc-en.js
+++ ULTRAbc-en.js
@@ -1083,7 +1083,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                 if (cname == null || cname == undefined) cname = "";
                 if (cowner1 == null || cowner1 == undefined) cowner1 = "";
                 if (cowner2 == null || cowner2 == undefined) cowner2 = 0;
-                if (cowner3 == null || cowner2 == undefined) cowner3 = 0;
+                if (cowner3 == null || cowner3 == undefined) cowner3 = 0;
                 if (cskeys == null || cskeys == undefined) cskeys = false;
                 if (csname == null || csname == undefined) csname = "Introduction";
                 if (ctitle == null || ctitle == undefined) ctitle = "";
@@ -15569,9 +15569,9 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                     let house = "";
                     if (ReputationGet(Player, "HouseAmplector") != 0) house = "HouseAmplector";
                     if (ReputationGet(Player, "HouseCorporis") != 0) house = "HouseCorporis";
-                    if (ReputationGet(PLayer, "HouseMaiestas") != 0) house = "HouseMaiestas";
+                    if (ReputationGet(Player, "HouseMaiestas") != 0) house = "HouseMaiestas";
                     if (ReputationGet(Player, "HouseVincula") != 0) house = "HouseVincula";
-                    if (ReputationGet(house) < 25) {
+                    if (house && ReputationGet(house) < 25) {
                         DialogSetReputation(house, 25);
                     }
                     TitleSet("Bishop");
@@ -16291,7 +16291,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                     "<b>/roleplay</b> (rolehere) = starts a role. *\n" +
                     "<b>/rolequit</b> (role or clubarea here) = ceases to play a role. *\n" +
                     "<b>/title1</b> (newtitlehere) = chooses a new title (from A to D). *\n" +
-                    "<b>/title1</b> (newtitlehere) = chooses a new title (from E to K). *\n" +
+                    "<b>/title2</b> (newtitlehere) = chooses a new title (from E to K). *\n" +
                     "<b>/title3</b> (newtitlehere) = chooses a new title (from L to Q). *\n" +
                     "<b>/title4</b> (newtitlehere) = chooses a new title (from R to Z). *";
                 infomsg(msg);
@@ -16771,7 +16771,7 @@ var bcModSDK=function(){"use strict";const o="1.2.0";function e(o){alert("Mod ER
                                 infomsg(msg);
                             }
                         }
-                        if ((Item.Asset.Name == "HeavyDutyBelt") || (Item.Asset.Name == "ModularChastityBelt") || (Item.Asset.Namer == "SciFiPleasurePanties")) {
+                        if ((Item.Asset.Name == "HeavyDutyBelt") || (Item.Asset.Name == "ModularChastityBelt") || (Item.Asset.Name == "SciFiPleasurePanties")) {
                             if ((mode > -1) && (mode < 10)) {
                                 if ((mode > -1) && (mode < 5)) {
                                     ExtendedItemSetOptionByRecord(Player, Item, {
